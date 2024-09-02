dc9a2f876d7a2f14e6fac1df899d5b164ce88cb7
Faisal Fawad <faisalfawad2004@gmail.com>
2024-09-01 21:52:58 -0400

    Update logging

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -8,6 +8,10 @@ PROFILE="dev"
 source .env
 
 # AWS build/deploy
+echo "Fetching bucket..."
 aws s3 mb s3://$BUCKET_NAME --region $REGION --profile $PROFILE
+echo "Packaging..."
 aws cloudformation package --template-file aws/deploy.yml --s3-bucket $BUCKET_NAME --output-template-file aws/packaged-deploy.yml --profile $PROFILE
+echo "Deploying..."
+ls aws
 aws cloudformation deploy --template-file aws/packaged-deploy.yml --stack-name $STACK_NAME --region $REGION --capabilities CAPABILITY_NAMED_IAM --profile $PROFILE --parameter-overrides mongoToken=$MONGO_TOKEN
