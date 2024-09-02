ed89723fb8006e7a91e74fb6805be7aa44440c62
Faisal Fawad <faisalfawad2004@gmail.com>
2024-09-01 21:56:57 -0400

    Deploy

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -11,7 +11,7 @@ source .env
 echo "Fetching bucket..."
 aws s3 mb s3://$BUCKET_NAME --region $REGION --profile $PROFILE
 echo "Packaging..."
-aws cloudformation package --template-file aws/deploy.yml --s3-bucket $BUCKET_NAME --output-template-file aws/packaged-deploy.yml --profile $PROFILE
+aws cloudformation package --template-file aws/deploy.yml --s3-bucket $BUCKET_NAME --output-template-file aws/packaged-deploy.yml --region $REGION --profile $PROFILE
 echo "Deploying..."
 ls aws
-aws cloudformation deploy --template-file aws/packaged-deploy.yml --stack-name $STACK_NAME --region $REGION --capabilities CAPABILITY_NAMED_IAM --profile $PROFILE --parameter-overrides mongoToken=$MONGO_TOKEN
+aws cloudformation deploy --template-file aws/packaged-deploy.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --region $REGION --profile $PROFILE --parameter-overrides mongoToken=$MONGO_TOKEN
