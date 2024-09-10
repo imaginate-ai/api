99d4577b3490606ad9aab10bd096ff227e344506
Faisal Fawad <76597599+faisal-fawad@users.noreply.github.com>
2024-09-10 07:54:10 -0400

    AWS rate limiting + production (#37)
    * AWS rate limiting + production
    
    * Update aws/deploy.yml
    
    Co-authored-by: Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nathan Probert <109328434+nathan-probert@users.noreply.github.com>

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -24,11 +24,12 @@ fi
 
 # AWS build/deploy
 echo "Fetching bucket..."
-aws s3 mb s3://$BUCKET_NAME --region $REGION --profile $PROFILE
+aws s3 mb s3://$BUCKET_NAME --region $REGION --profile $PROFILE # Make bucket (if it does not exist)
+aws s3 rm s3://$BUCKET_NAME --region $REGION --profile $PROFILE --recursive # Delete all objects
 echo "Packaging..."
-aws cloudformation package --template-file aws/deploy.yml --s3-bucket $BUCKET_NAME --output-template-file aws/packaged-deploy.yml --region $REGION --profile $PROFILE
+aws cloudformation package --template-file aws/deploy.yml --s3-bucket $BUCKET_NAME --output-template-file aws/packaged-deploy.yml --region $REGION --profile $PROFILE # Compile template
 ls aws
 echo "Deploying..."
-aws cloudformation deploy --template-file aws/packaged-deploy.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --region $REGION --profile $PROFILE --parameter-overrides mongoToken=$MONGO_TOKEN
+aws cloudformation deploy --template-file aws/packaged-deploy.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --region $REGION --profile $PROFILE --parameter-overrides mongoToken=$MONGO_TOKEN # Deploy with template
 echo "Check status..."
-aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --profile $PROFILE --query "Stacks[0].StackStatus" --output text
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --profile $PROFILE --query "Stacks[0].StackStatus" --output text # Output stack status
